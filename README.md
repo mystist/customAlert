4919859d82dd27fa267b1185e77437ba09202750
Liber <gjl87910lq@hotmail.com>
2013-05-09 09:50:03 +0800

    customAlert
    1

---
+++ README.md
@@ -0,0 +1,24 @@
+# Custom your Alert & Confirm
+
+customAlert can help you custom your Alert & Confirm with your own CSS and HTML.
+
+You can fell free to use it, modify it and share it with your friends.
+
+Contact me for more information:  
+
+[http://mystist.github.com/][0]  
+
+[0]: http://mystist.github.com/
+
+# Change Log
+
+## 2013-05-09  
+Initial customAlert release.
+
+
+
+
+
+
+
+
---
+++ customAlert.js
@@ -0,0 +1,69 @@
+﻿/*
+ * customAlert
+ * 
+ * https://github.com/Mystist/customAlert
+ *
+ * Copyright (c) 2013 Foundation and other contributors
+ *
+ * License: https://github.com/Mystist/customAlert/blob/master/MIT-LICENSE.txt
+ *
+ */
+
+(function($) {
+
+  var methods = {
+
+    init: function() {
+    
+      var $this = this;
+      window.alert = function(str) {
+        $this.find(".message").html(str);
+        $this.find(".close").unbind("click").bind("click", function() {
+          $this.hide();
+        });
+        $this.show();
+      }
+      
+    },
+    
+    confirm: function(options) {
+    
+      var defaults = {
+        "message": "Confirm?",
+        "yes": function() {},
+        "no": function() {}
+      };
+      var settings = $.extend(defaults, options);
+      
+      var $this = this;
+      
+      $this.find(".message").html(settings.message);
+      $this.find(".close").unbind("click").bind("click", function() {
+        $this.hide();
+      });
+      $this.find(".yes").unbind("click").bind("click", function() {
+        settings.yes();
+        $this.hide();
+      });
+      $this.find(".no").unbind("click").bind("click", function() {
+        settings.no();
+        $this.hide();
+      });
+      
+      $this.show();
+    
+    }
+
+  };
+
+  $.fn.customAlert = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || !method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('No ' + method + ' method.');
+    }
+  };
+
+})(jQuery);
